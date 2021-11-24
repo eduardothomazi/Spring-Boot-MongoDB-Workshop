f32b76fc3034769a211e15e8d2049b9f109b3924
Eduardo Thomazi <eduardothomazi91@gmail.com>
2021-11-24 19:53:02 -0300

    C8 - Projecting an Author in a Post obj using AuthorDTO class

--- src/main/java/com/eduardo/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/eduardo/workshopmongodb/config/Instantiation.java
@@ -1,5 +1,6 @@
 package com.eduardo.workshopmongodb.config;
 
+import com.eduardo.workshopmongodb.dto.AuthorDTO;
 import com.eduardo.workshopmongodb.entities.Post;
 import com.eduardo.workshopmongodb.entities.User;
 import com.eduardo.workshopmongodb.repositories.PostRepository;
@@ -22,19 +23,21 @@ public class Instantiation implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
         userRepository.deleteAll();
         postRepository.deleteAll();
+
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria,alex,bob));
+        //saving users first is needed because if you create a Post obj the author id will be null
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!",new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
         postRepository.saveAll(Arrays.asList(post1,post2));
     }
 }
--- src/main/java/com/eduardo/workshopmongodb/entities/Post.java
+++ src/main/java/com/eduardo/workshopmongodb/entities/Post.java
@@ -1,5 +1,7 @@
 package com.eduardo.workshopmongodb.entities;
 
+import com.eduardo.workshopmongodb.dto.AuthorDTO;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
@@ -14,14 +16,14 @@ public class Post implements Serializable {
     private String title;
     private String body;
 
-    private User author;
+    private AuthorDTO author;
 
     private List<Comment> comments = new ArrayList<>();
 
     public Post() {
     }
 
-    public Post(String id, Date date, String title, String body, User author) {
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
         this.id = id;
         this.date = date;
         this.title = title;
@@ -61,11 +63,11 @@ public class Post implements Serializable {
         this.body = body;
     }
 
-    public User getAuthor() {
+    public AuthorDTO getAuthor() {
         return author;
     }
 
-    public void setAuthor(User author) {
+    public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
 
