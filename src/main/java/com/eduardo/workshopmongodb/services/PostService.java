9abdb4afbd672f65703b7eb6c1cc958aea8291b0
Eduardo Thomazi <eduardothomazi91@gmail.com>
2021-11-24 20:59:30 -0300

    C11 - findById() function for Post

--- src/main/java/com/eduardo/workshopmongodb/services/PostService.java
+++ src/main/java/com/eduardo/workshopmongodb/services/PostService.java
@@ -1,7 +1,25 @@
 package com.eduardo.workshopmongodb.services;
 
+import com.eduardo.workshopmongodb.entities.Post;
+import com.eduardo.workshopmongodb.repositories.PostRepository;
+import com.eduardo.workshopmongodb.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id){
+         Optional<Post> post = postRepository.findById(id);
+         if (post == null){
+             throw new ObjectNotFoundException("Object not found!");
+         }
+         return post.get();
+
+    }
 }
