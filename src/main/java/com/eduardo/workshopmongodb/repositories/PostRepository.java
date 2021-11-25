35274a316ae8dfcd335f1217a100f95d65d857e7
Eduardo Thomazi <eduardothomazi91@gmail.com>
2021-11-25 13:04:40 -0300

    C13 - Implementing Spring query methods - Post -> findByTitle()

--- src/main/java/com/eduardo/workshopmongodb/repositories/PostRepository.java
+++ src/main/java/com/eduardo/workshopmongodb/repositories/PostRepository.java
@@ -3,5 +3,8 @@ package com.eduardo.workshopmongodb.repositories;
 import com.eduardo.workshopmongodb.entities.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface PostRepository extends MongoRepository<Post, String> {
+    List<Post> findByTitleContainingIgnoreCase(String text);//Spring Boot query methods
 }
